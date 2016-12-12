c6e3f45178cac3171b17552162a0de7b82672e66
Madeline <madeline@done-digital.com>
2016-12-11 16:28:08 -0800

    Creating a custom infinite scroll

--- CodePen/gallery/js/main.js
+++ CodePen/gallery/js/main.js
@@ -25,22 +25,61 @@ var source = $(".gallery").html();
 var template = Handlebars.compile(source);
 //Create the fill in data
 
-//Append it to your document
-for (x=0; x<3; x++) {
+// //Append it to your document
+// for (x=0; x<3; x++) {
 
-	var data = {
-			url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVay3iVUL1fN_ak4WlUA64yAe6IxX8YZHXQd_aX-lYlBEs4J9F',
-			alt:"Person",
-			index: x,
-			click: function(index){
-				console.log(data.alt);
-			}
-	};
-	//Send the data in to the template
-	var newListItemHTML = template(data);
-	$('body').append(newListItemHTML);
-}
+// 	var data = {
+// 			url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVay3iVUL1fN_ak4WlUA64yAe6IxX8YZHXQd_aX-lYlBEs4J9F',
+// 			alt:"Person",
+// 			index: x,
+// 			click: function(index){
+// 				console.log(data.alt);
+// 			}
+// 	};
+// 	//Send the data in to the template
+// 	var newListItemHTML = template(data);
+// 	$('body').append(newListItemHTML);
+// }
+
+//Make request to tumblr
+var request = $.ajax({  url: "http://api.tumblr.com/v2/blog/natalieomoore.tumblr.com/posts?api_key=V0EUYuGTiPOFIY0gxZbuOJTIUYwPQX49i6wOHUkwrwiNfCfjtw&limit=50",
+    dataType: 'jsonp',
+    success: function(posts){
+    	for (x=0; x<10; x++ ) {
+    		var imageURL = posts.response.posts[x].photos[0].original_size.url
+	    	$('body').append('<img src="'+imageURL+'">');
+
+	    }
+    	
+    	x=11;
+    	$(window).scroll(function() {
+	    	 if($(window).scrollTop() + $(window).height()-100 === $(document).height()-100) {
+	    	 		// y = 0;
+		      	// 	for (x=y; x<y+10; x++ ) {
+
+			    			// console.log(posts.response.posts.length);
+			    		var imageURL = posts.response.posts[x].photos[0].original_size.url
+			    		$('body').append('<img class="new" src="'+imageURL+'">');
+			    			
+			    		x+=1;
+			    		console.log(x);
+			    		// y+=10;
+
+		    		// }
+	   		}	
+    	});
+    	
+    },
+    error: function() {
+    	alert('error getting tumblr data');
+    }
+});
 
 
+$('img').addClass('lazy');
 
+$("img.lazy").lazyload({
+    effect : "fadeIn",
+    threshold : -100
+});
 	
